cf3ab4bbfa6945db0b255d0572733e6999c38c41
pedroeroel <pedroedroel@gmail.com>
2025-06-06 12:31:40 -0300

    feat: login, register, index and home pages

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, Blueprint
 from flask_cors import CORS
 from app.routes.main.routes import main
+from app.routes.auth.routes import auth
 
 def create_app():
 
@@ -11,5 +12,6 @@ def create_app():
     CORS(app, resources=sources)
 
     app.register_blueprint(main)
+    app.register_blueprint(auth)
 
     return app
--- app/routes/main/routes.py
+++ app/routes/main/routes.py
@@ -2,12 +2,12 @@ from flask import Flask, Blueprint, request, render_template, session
 
 main = Blueprint('main', __name__, template_folder='templates')
 
+session = True
+
 @main.route('/')
 def index():
-
-    session = False
 
     if session:
         return render_template('home.html')
     else:
-        return render_template('index.html')
+        return render_template('index.html')    
