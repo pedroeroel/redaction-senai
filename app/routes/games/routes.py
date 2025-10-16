70fd9d568a4c666444c7ee9522fdf7f22d59e61b
pedroeroel <pedroedroel@gmail.com>
2025-10-16 12:11:07 -0300

    feat: game pages added

--- app/routes/games/routes.py
+++ app/routes/games/routes.py
@@ -8,3 +8,10 @@ def games_list():
         return render_template('games.html')
     else:
         return redirect('/login')
+    
+@games.route('/games/<int:id>', methods=['GET'])
+def games_page(id):
+    if session:
+        return render_template(f'games/game_{id}.html')
+    else:
+        return redirect('/login')
